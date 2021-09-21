457c666791695653c0301149bada885d290f143b
Jun-T-git <jun0303t@icloud.com>
2021-09-21 18:39:42 +0900

    socket.ioの設定追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,23 @@
-import { useState } from "react";
+import { useEffect } from "react";
+import socketIOClient from "socket.io-client";
 import dynamic from "next/dynamic";
+
 const PDFViewer = dynamic(() => import("~/components/pdfViewer"), {
   ssr: false,
 });
 
+const ENDPOINT = process.env.NEXT_PUBLIC_ENDPOINT || "http://localhost:3001";
+
 const Index: React.VFC = () => {
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on("connected", (data) => console.log("connected"));
+    socket.on("moved", (data) => console.log("moved"));
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
     <>
       <div className="flex">
